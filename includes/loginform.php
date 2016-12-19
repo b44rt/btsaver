577407ec91c6fb6e6d9b0aabb03f975db2a6c7e6
Bart <bart.huijsmans@gmail.com>
2016-12-19 16:00:24 +0100

    Add files via upload
    Added a login form
    prepared login system

---
+++ includes/loginform.php
@@ -0,0 +1,5 @@
+<form action="login.php" method="POST">
+<input type="text" name="username" maxlength="255" autofocus required/>
+<input type="password" name="password" maxlength="255" required/>
+<input type="submit" name="submit" value="submit"/>
+</form>
