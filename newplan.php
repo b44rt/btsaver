fa7d040e8d568784a18df521036e9fed4f6f404b
Bart <bart.huijsmans@gmail.com>
2016-12-30 15:27:53 +0100

    Add files via upload
    Sanitized input, convert BTC input to Satoshi Amount to save as INT

--- newplan.php
+++ newplan.php
@@ -13,10 +13,11 @@ include('functions.php');
 //Verwerk dit nog naar een te callen functie,  ook moet loginsysteem afgemaakt worden zodat dit per user gedaan kan worden.
 if(!empty($_POST['submit']))
 {
-	$plantype = $_POST['plantype'];
-	$btcaddress = $_POST['btcaddress'];
-	$goal = $_POST['goal'];
-	$descr = $_POST['descr'];
+	$plantype = mysqli_real_escape_string($conn, $_POST['plantype']);
+	$btcaddress = mysqli_real_escape_string($conn, $_POST['btcaddress']);
+	$goal = mysqli_real_escape_string($conn, $_POST['goal']);
+	$goal = $goal * 100000000;//Maak Satoshi Amount om als INT op te slaan
+	$descr = mysqli_real_escape_string($conn, $_POST['descr']);
 
 	
 	$sql = "INSERT INTO plans (plantype, btcaddress, goal, descr, creation)
@@ -42,11 +43,11 @@ else
 <div id="wrap">
 <h2>New Plan</h2>
 <table>
-<form action="#" method="POST">
-<tr><td>Track as Saving: </td><td><input type="radio" name="plantype" value="0" checked/></td></tr>
-<tr><td>Track as Investment: </td><td><input type="radio" name="plantype" value="1"/></td></tr>
+<form action="newplan.php" method="POST">
+<tr><td>Track as Saving: </td><td><input type="radio" name="plantype" value=0 checked/></td></tr>
+<tr><td>Track as Investment: </td><td><input type="radio" name="plantype" value=1/></td></tr>
 <tr><td>Bitcoin Address: </td><td><input type="text" name="btcaddress" pattern=".{26,34}" required title="A Valid Bitcoin Public Key"/></td></tr>
-<tr><td>Goal/Invested (BTC): </td><td><input type="number" min="0" max="21000000" name="goal"/></td></tr>
+<tr><td>Goal/Invested (BTC): </td><td><input type="number" min="0" max="21000000" step="0.00000001" name="goal"/></td></tr>
 <tr><td>Description: </td><td><input type="text" name="descr"/></td></tr>
 <tr><td colspan="2"><input type="submit" name="submit" value="submit"/></td></tr>
 </form>
